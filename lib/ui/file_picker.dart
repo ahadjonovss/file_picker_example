4230157174432b396559fed2f1b9259047674d22
Samandar <samandarahadjonov@gmail.com>
2022-12-22 14:50:26 +0500

    File Picker done

--- lib/ui/file_picker.dart
+++ lib/ui/file_picker.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,8 +10,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  File? selectedFile;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+
+    Future<File?> selectFile() async {
+      FilePickerResult? result = await FilePicker.platform.pickFiles();
+      if(result!=null){
+        return File(result.files.single.path!);
+      }
+      return null;
+
+    }
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            ElevatedButton(
+                onPressed: () async{
+              File? myFile= await selectFile();
+
+              if(myFile!=null){
+                selectedFile=myFile;
+                setState(() {
+                });
+              }
+            }, child: Text("Select file")),
+            selectedFile!=null? Image.file(selectedFile!,height: 300,):SizedBox(),
+          ],
+        ),
+      ),
+    );
   }
 }
