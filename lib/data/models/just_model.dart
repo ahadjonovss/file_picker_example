11013fddbc883ba136ab1502dad0b60114577ef2
Samandar <samandarahadjonov@gmail.com>
2022-12-22 14:59:41 +0500

    model done

---
+++ lib/data/models/just_model.dart
@@ -0,0 +1,53 @@
+class JustModel{
+  // "code": "currency",
+  // "caption": "Currency",
+  // "fullCaption": "Enter Currency",
+  // "sort": 9,
+  // "group": "COMMON",
+  // "initial_value": "USD",
+  // "regExp": "",
+  // "required": true,
+  // "type": "select",
+  // "values":[
+  // "USD",
+  // "SO'M",
+  // "RUBL"
+  // ]
+
+  String code;
+  String caption;
+  String fullCaption;
+  num sort;
+  String group;
+  String initial_value;
+  String regExp;
+  bool required;
+  String type;
+  List<String> values;
+
+  JustModel({
+    required this.required,
+    required this.type,
+    required this.values,
+    required this.caption,
+    required this.code,
+    required this.fullCaption,
+    required this.group,
+    required this.initial_value,
+    required this.regExp,
+    required this.sort
+});
+
+  factory JustModel.fromJson(Map<String, dynamic> json) => JustModel(
+    code: json["code"]??"",
+    caption: json["caption"]??"",
+    fullCaption: json["fullCaption"]??"",
+    sort: json["sort"]??0,
+    group: json["group"]??"",
+    regExp: json["regExp"]??"",
+    required: json["required"]??true,
+    type: json["type"]??"",
+    values: json["values"]??[],
+    initial_value: json["initial_value"]??"",
+  );
+}
